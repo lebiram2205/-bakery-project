8a4cdbc8e19673ce9474bc3bc0c427c31deae17a
David <chavezzday@gmail.com>
2021-06-08 21:40:22 -0500

    starting with css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import style from "./style.css"
-import logo from "./assets/prueba2.svg"
-const arr =[1,2,3],
-codeESNext=()=>console.log(...arr);
-console.log("hola mundo sin configurar con webpack");
-codeESNext();
-alert("hola");
+import './style.css'; 
